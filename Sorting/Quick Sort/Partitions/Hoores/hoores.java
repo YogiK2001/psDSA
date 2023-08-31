e24a25c98db44b00a66e57d9c0cfb6261eb26b17
YogiK2001 <kaustubh7718@gmail.com>
2023-08-31 18:16:37 +0530

    added comments

--- Sorting/Quick Sort/Partitions/Hoores/hoores.java
+++ Sorting/Quick Sort/Partitions/Hoores/hoores.java
@@ -1,3 +1,15 @@
+/**
+ * Hoare's Partition Algorithm
+ *
+ * This algorithm is used to partition an array around a chosen pivot element,
+ * such that all elements less than or equal to the pivot are on its left side,
+ * and all elements greater than the pivot are on its right side.
+ *
+ * @param arr The array to be partitioned.
+ * @param l   The index of the left boundary of the partition.
+ * @param h   The index of the right boundary of the partition.
+ * @return The index of the pivot element after partitioning.
+ */
 
 public class hoores {
 
@@ -11,18 +23,27 @@ public class hoores {
     }
 
     public static int partition(int arr[], int l, int h) {
-        int pivot = arr[l];
+        int pivot = arr[l]; // Select the pivot element from the leftmost position
+
         int i = l - 1;
         int j = h + 1;
+
         while (true) {
+            // Increment i until arr[i] is not less than pivot
             do {
                 i++;
             } while (arr[i] < pivot);
+
+            // Decrement j until arr[j] is not greater than pivot
             do {
                 j--;
             } while (arr[j] > pivot);
+
+            // If i and j cross each other, the partitioning is complete
             if (i >= j)
                 return j;
+
+            // Swap arr[i] and arr[j] to maintain the partition
             swap(arr, i, j);
         }
     }
@@ -35,3 +56,29 @@ public class hoores {
         arr[j] = temp;
     }
 }
+
+/*
+ * Practical Use Cases:
+ * 
+ * Hoare's partition algorithm is commonly used in various scenarios similar to
+ * Lomuto's partition. Here are some of its practical use cases:
+ * 
+ * Quicksort: Hoare's partition algorithm is used as a key component of the
+ * quicksort algorithm, one of the fastest sorting algorithms.
+ * 
+ * Order Statistics: Just like Lomuto's partition, it can be used to find the
+ * k-th smallest or largest element in an array.
+ * 
+ * Data Analysis: In statistical analyses and data processing, sorting is often
+ * required to identify medians and quartiles.
+ * 
+ * Binary Search: Some binary search algorithms work with sorted arrays, and
+ * quicksort, using Hoare's partition, can help achieve this sorted order.
+ * 
+ * Load Balancing: In load balancing algorithms, efficient data partitioning is
+ * crucial, and quicksort's partitioning strategy can be employed.
+ * 
+ * Duplicate Detection: Similar to Lomuto's partition, it can be used to detect
+ * duplicate elements.
+ * 
+ */
